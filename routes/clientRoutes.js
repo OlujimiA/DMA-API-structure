0733afecbc782839123b0aea4beab85d7a58f4dc
widespread0-0 <danielolugbule@gmail.com>
2025-06-13 00:24:22 +0100

    clientRoutes.js

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const clientController = require('../controllers/clientController');
+
+router.get('/', clientController.getAllClients);
+
+router.post('/', clientController.createClient);
+
+router.put('/:id', clientController.updateClient);
+
+router.delete('/:id', clientController.deleteClient);
+
+module.exports = router;
 
