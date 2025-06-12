16190b557d74cfa60efcb822b72e9f239ea2f584
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-12 22:41:24 +0100

    Edit clientController pathway

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const clientController = require('../controllers/clientController');
+const clientController = require('../controllers/clientController.js');
 
 router.get('/', clientController.getAllClients);
 
