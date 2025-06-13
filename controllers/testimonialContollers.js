370f154e426f690a4b5a9891c6d7f336ccc25137
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-13 13:16:24 +0100

    Deleted duplicate contollers files

--- controllers/testimonialContollers.js
+++
@@ -1,7 +0,0 @@
-const testimonial = require('../testimonials/testimonials.js');
-
-exports.getTestimonialByOrgId = async (req, res) => {
-  const testimonial = await testimonial.getTestimonialByOrgId(req.params.id);
-  if (!testimonial) return res.status(404).json({ message: 'testimonial not found' });
-  res.json(testimonial);
-};
