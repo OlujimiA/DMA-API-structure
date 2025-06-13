dc31670d7134434f752da06a564370b1678c7a00
widespread0-0 <danielolugbule@gmail.com>
2025-06-13 01:09:53 +0100

    leadRoutes.js

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const leadController = require('../controllers/leadController');
 
-router.get('/:organisation_id', leadController.getLeadsByOrganisation);
-
-router.post('/', leadController.createLead);
-
-module.exports = router;
