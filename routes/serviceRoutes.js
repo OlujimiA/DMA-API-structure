2683067bf726ac0c11a29d95106d115677f78869
widespread0-0 <danielolugbule@gmail.com>
2025-06-13 01:08:57 +0100

    serviceRoutes.js

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const serviceController = require('../controllers/serviceController');
 
-router.get('/:organisation_id', serviceController.getServicesByOrganisation);
-
-router.post('/', serviceController.createService);
-
-router.put('/:id', serviceController.updateService);
-
-router.delete('/:id', serviceController.deleteService);
-
-module.exports = router;
