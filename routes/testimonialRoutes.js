1d3cfcb56e16e423a7f7dee7fe8576060cd20d03
widespread0-0 <danielolugbule@gmail.com>
2025-06-13 01:08:39 +0100

    testimonialRoutes.js

--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const testimonialController = require('../controllers/testimonialController');
 
-router.get('/:organisation_id', testimonialController.getTestimonialsByOrganisation);
-
-router.post('/', testimonialController.createTestimonial);
-
-router.put('/:id', testimonialController.updateTestimonial);
-
-router.delete('/:id', testimonialController.deleteTestimonial);
-
-module.exports = router;
