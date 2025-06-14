e25da3509ada65659a8973dc21dfcd0f1803999f
widespread0-0 <danielolugbule@gmail.com>
2025-06-14 09:50:22 +0100

    Update authModels.js

--- models/authModels.js
+++ models/authModels.js
@@ -1,1 +1,11 @@
+const db = require('../config/db');
+
+const getUserByEmail = async (email) => {
+  const [rows] = await db.execute('SELECT * FROM users WHERE email = ?', [email]);
+  return rows[0]; 
+};
+
+module.exports = {
+  getUserByEmail,
+};
 
