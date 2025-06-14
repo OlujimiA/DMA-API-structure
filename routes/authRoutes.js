ea649d0293555fde35cd80bf5a063bc891c9bd5c
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-14 19:50:19 +0100

    Add signup route to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const authControllers = require('../controllers/authControllers');
 
-
 router.post('/login', authControllers.login);
+router.post('/signup', authControllers.signup);
 module.exports = router;
 
