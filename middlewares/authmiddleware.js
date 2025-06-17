77b2d8a9b1c386ca5ec966e6af2ec390e5ed5442
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-17 13:38:09 +0100

    Update auth middleware const name and status code

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -7,10 +7,10 @@ module.exports = (req, res, next) => {
   const token = authHeader.split(' ')[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
     req.user = decoded; // attach user to request
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Invalid or expired token' });
+    res.status(403).json({ message: 'Invalid or expired token' });
   }
 };
