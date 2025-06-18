560986aa13d771424d86486bd5caa16d1ed6216d
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-18 12:33:06 +0100

    Add prisma connection in db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,4 @@
-require('dotenv').config();
-const mysql = require('mysql2/promise');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
-
-module.exports = db;
+module.exports = prisma;
