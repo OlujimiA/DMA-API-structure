a3c698abc267b412486963f0902de6a5846280ba
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-18 13:40:53 +0100

    Edit clientModel with prisma

--- models/clientModels.js
+++ models/clientModels.js
@@ -1,41 +1,60 @@
-const db = require('../config/db');
+const prisma = require('../config/db');
 
 const getAllclients = async () => {
-  const [rows] = await db.query('SELECT id, name, email, tel AS phone_number, country FROM client');
-  return rows;
+  const clients = await prisma.client.findMany({
+    omit: {
+      password: true,
+    },
+  });
+  return clients;
 };
 
 const getclientById = async (id) => {
-  const [rows] = await db.query('SELECT id, name, email, tel AS phone_number, country FROM client WHERE id = ?', [id]);
-  return rows;
+  const client = await prisma.client.findUnique({
+    where: { id: parseInt(id) },
+    omit: {
+      password: true,
+    },
+  });
+  return client;
 };
 
 const createClient = async ({ name, email, tel, country, password }) => {
-  const [result] = await db.query(
-    `INSERT INTO client (name, email, tel, country, password)
-     VALUES (?, ?, ?, ?, ?)`,
-    [name, email, tel, country, password]
-  );
-  
-  return {
-    id: result.insertId,
-    name,
-    email,
-    tel
-  };
-};
+  const result = await prisma.client.create({
+    data: {
+      name: name, email: email, tel: tel, country: country, password: password,
+    },
+    
+  });
 
-const deleteClient = async (id) => {
-  const [result] = await db.execute(
-    `DELETE FROM client WHERE id = ?`,
-    [id]
-  );
-  return result; 
-};
+}
+
+// const createClient = async ({ name, email, tel, country, password }) => {
+//   const [result] = await db.query(
+//     `INSERT INTO client (name, email, tel, country, password)
+//      VALUES (?, ?, ?, ?, ?)`,
+//     [name, email, tel, country, password]
+//   );
+  
+//   return {
+//     id: result.insertId,
+//     name,
+//     email,
+//     tel
+//   };
+// };
+
+// const deleteClient = async (id) => {
+//   const [result] = await db.execute(
+//     `DELETE FROM client WHERE id = ?`,
+//     [id]
+//   );
+//   return result; 
+// };
 
 module.exports = {
   getAllclients,
   getclientById,
   createClient,
-  deleteClient,
+  // deleteClient,
 };
