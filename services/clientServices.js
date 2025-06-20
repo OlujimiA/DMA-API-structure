bc491463b018b674fcfe16b29fd03da0c4ca1222
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-20 22:34:43 +0100

    delete authServices and add login to clientservices

--- services/clientServices.js
+++ services/clientServices.js
@@ -1,4 +1,30 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const clientModel = require('../models/clientModels.js');
+
+const login = async (email, password) => {
+  const user = await authModel.getUserByEmail(email);
+  if (!user) return null;
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) return null;
+
+  // create token
+  const accessToken = jwt.sign(
+    { id: user.id, email: user.email, role: user.role },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: '1h' }
+  );
+
+  const refreshToken = jwt.sign(
+    { id: user.id },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: '7d' }
+  );
+
+  return { tokens: {accessToken, refreshToken}, user: { id: user.id, email: user.email, role: user.role } };
+};
+
 
 const getAllclients = async () => {
   return await clientModel.getAllclients();
