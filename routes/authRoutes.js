1098d9af289eed841f6e7b920add455378f9e057
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-20 22:23:43 +0100

    remove login and signup endpoints

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,7 @@
-
 const express = require('express');
 const router = express.Router();
 const authControllers = require('../controllers/authControllers');
 
-router.post('/login', authControllers.login);
-router.post('/signup', authControllers.signup);
 router.post('/refresh', authControllers.refreshToken)
 module.exports = router;
 
