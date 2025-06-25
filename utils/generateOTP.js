da656e710dfb5c0a3eea07a4b35ec2aaf62e7bfd
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-25 15:16:40 +0100

    Create OTP generator in utilities folder

---
+++ utils/generateOTP.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto');
+
+function generateOTP(length = 6) {
+  let otp = '';
+  for (let i = 0; i < length; i++) {
+    otp += crypto.randomInt(0, 10);
+  }
+  return otp;
+}
+
+module.exports = generateOTP;
