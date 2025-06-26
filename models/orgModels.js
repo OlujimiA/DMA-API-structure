be5fa71d5da62ecadd2b73de272cf49cfe301c62
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-26 15:06:12 +0100

    Edit create and update org models

--- models/orgModels.js
+++ models/orgModels.js
@@ -1,55 +1,46 @@
 const prisma = require('../config/db');
 
 const getAllorgs = async () => {
-  const orgs = await prisma.organisation.findMany({
-    omit: {
-      password: true,
-    },
-  });
+  const orgs = await prisma.organisation.findMany();
   return orgs;
 };
 
 const getorgById = async (id) => {
   const org = await prisma.organisation.findUnique({
     where: {id: parseInt(id)},
-    omit: {
-      password: true,
-    },
   });
   return org;
 };
 
-const createOrg = async ({ name, email, tel, website, address, password }) => {
+const createOrg = async ({ name, email, address, country, type, industry, rc_number, logo_url }) => {
   const org = await prisma.organisation.create({
     data: {
       name: name, 
-      email: email, 
-      tel: tel, 
-      website: website, 
-      address: address, 
-      password: password,
+      email: email,
+      address: address,  
+      country: country,  
+      type: type,
+      industry: industry,
+      rc_number: rc_number,
+      logo_url: logo_url,
     },
-    omit: {
-      password: true,
-    }
   });
   return org;
 };
 
-const updateOrg = async (id, { name, email, tel, website, address, password }) => {
+const updateOrg = async (id, { name, email, address, country, type, industry, rc_number, logo_url }) => {
   const updated = await prisma.organisation.update({
     where: { id: parseInt(id) },
     data: {
       name: name, 
-      email: email, 
-      tel: tel, 
-      website: website, 
-      address: address, 
-      password: password,
+      email: email,
+      address: address,  
+      country: country,  
+      type: type,
+      industry: industry,
+      rc_number: rc_number,
+      logo_url: logo_url,
     },
-    omit: {
-      password: true,
-    }
   });
   return updated;
 };
@@ -57,9 +48,6 @@ const updateOrg = async (id, { name, email, tel, website, address, password }) =
 const deleteOrg = async (id) => {
   const deleted = await prisma.organisation.delete({
     where: { id: parseInt(id) },
-    omit: {
-      password: true,
-    }
   });
   return deleted; 
 };
