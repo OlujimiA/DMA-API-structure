5cd6ce9544b2cda15528cae01748495b630b37df
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-26 11:46:35 +0100

    Add error handler and expiry to function

--- utils/generateOTP.js
+++ utils/generateOTP.js
@@ -1,11 +1,16 @@
 const crypto = require('crypto');
 
 const generateOTP = async () => {
-  let otp = '';
-  for (let i = 0; i < 6; i++) {
-    otp += crypto.randomInt(0, 10);
+  try {
+    let otp = '';
+    for (let i = 0; i < 6; i++) {
+      otp += crypto.randomInt(0, 10);
+    }
+    const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 mins
+    return { otp, expiresAt };
+  } catch (err) {
+    throw new Error("Error generating OTP: " + err.message);
   }
-  return otp;
 };
 
 module.exports = generateOTP;
