423ba3c2f069cf825fda3fd30cfb899d255cdd78
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-06-27 15:18:49 +0100

    Create and order routes

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -4,8 +4,8 @@ const auth = require('../middlewares/authmiddleware');
 const clientController = require('../controllers/clientControllers');
 
 router.get('/', auth, clientController.getAllclients);
-router.get('/:id', auth, clientController.getclientById);
 router.post('/profile/:id', auth, clientController.profile);
+router.get('/:id', auth, clientController.getclientById);
 router.put('/:id', auth, clientController.updateClient);
 router.delete('/:id', auth, clientController.deleteClient);
 
--- routes/orgRoutes.js
+++ routes/orgRoutes.js
@@ -4,8 +4,9 @@ const auth = require('../middlewares/authmiddleware');
 const orgController = require('../controllers/orgControllers');
 
 router.get('/', auth, orgController.getAllorgs);
+router.get('/contact', auth, orgController.getAllContacts);
+router.get('/contact/:id', auth, orgController.getContact);
 router.get('/:id', auth, orgController.getorgById);
-router.get('/contact', auth, orgController.getContact);
 router.post('/', auth, orgController.createOrg);
 router.post('/contact', auth, orgController.createContact);
 router.put('/:id', auth, orgController.updateOrg);
