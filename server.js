d3d722e819ad4acb53fb87a8b5552ef09c8bf0a7
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-07-10 11:04:09 +0100

    Delete lead files from API

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orgRoutes = require('./routes/orgRoutes');
-const leadRoutes = require('./routes/leadRoutes');
 const testimonialRoutes = require('./routes/testimonialRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
 
@@ -18,7 +17,7 @@ app.use(cookieParser());
 app.use('/api/users', userRoutes);
 app.use('/api/orgs', orgRoutes);
 app.use('/api/testimonials', testimonialRoutes);
-app.use('/api/leads', leadRoutes);
+app.use('/api/services', serviceRoutes);
 app.use('/api/auth', authRoutes)
 
 const PORT = process.env.PORT || 3000;
