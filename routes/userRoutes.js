75651cf4f822549e6ca808a003bf8646fe76a12c
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-07-11 22:35:19 +0100

    Reserve delete route to admins

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/authmiddleware');
 const userController = require('../controllers/userControllers');
+const authenticate = require('../middlewares/authenticate');
 
 router.get('/', auth, userController.getAllusers);
 router.post('/profile/:id', auth, userController.profile);
 router.get('/:id', auth, userController.getuserById);
 router.put('/:id', auth, userController.updateUser);
-router.delete('/:id', auth, userController.deleteUser);
+router.delete('/:id', authenticate.authorizeRoles('admin'), auth, userController.deleteUser);
 
 module.exports = router;
