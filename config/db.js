d986b2d77519b0c6e04b74cad3b6cdc4292b1835
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-07-11 22:13:54 +0100

    Add default user role id to config

--- config/db.js
+++ config/db.js
@@ -1,4 +1,9 @@
 const { PrismaClient } = require('../generated/prisma');
 const prisma = new PrismaClient();
 
-module.exports = prisma;
+const defaultUserRoleId = '9b3b3831-c43e-489a-8f35-faa42494ad22'
+
+module.exports = {
+    prisma,
+    defaultUserRoleId,
+};
