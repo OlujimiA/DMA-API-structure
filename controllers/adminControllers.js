8b03fdacada5d90147b9c1aac89c5573591b1514
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-07-15 22:16:09 +0100

    Fix syntax to check for an empty array

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -36,7 +36,7 @@ exports.getAdmin = async (req, res) => {
 exports.getRoles = async (req, res) => {
     try {
         const roles = await adminService.getRoles();
-        if (!roles) return res.status(404).json({ message: 'roles not found' });
+        if (roles.length===0) return res.status(404).json({ message: 'roles not found' });
         res.status(200).json(roles);
     } catch (err) {
         res.status(500).json({ message: 'Could not get roles', Error: err.message});
