af36299de32ad1ce9935e20b132111a78f40109c
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-07-15 21:13:06 +0100

    Edit controller function and add roles auth
    Change getTestimonialByOrgId to getTestimonialById
    to fetch testimonials by its id.
    
    Add Admin authorization to testimonial delete
    route.

--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/authmiddleware');
 const testimonialController = require('../controllers/testimonialControllers');
+const { authorizeRoles } = require('../middlewares/authenticate')
 
 router.get('/', auth, testimonialController.getAlltestimonials);
-router.get('/:id', auth, testimonialController.getTestimonialByOrgId);
+router.get('/:id', auth, testimonialController.getTestimonialById);
 router.post('/', auth, testimonialController.createTestimonial);
 router.put('/:id', auth, testimonialController.updateTestimonial);
-router.delete('/:id', auth, testimonialController.deleteTestimonial);
+router.delete('/:id', authorizeRoles('admin'), auth, testimonialController.deleteTestimonial);
 
 module.exports = router;
 
