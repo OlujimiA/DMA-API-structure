d6721befa7ece4164bf7668debf437ad4f46c7a2
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-07-26 16:05:33 +0100

    Add Service request route to API

--- server.js
+++ server.js
@@ -10,17 +10,19 @@ const userRoutes = require('./routes/userRoutes');
 const orgRoutes = require('./routes/orgRoutes');
 const testimonialRoutes = require('./routes/testimonialRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
+const requestRoutes = require('./routes/requestRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 
 app.use(express.json());
 app.use(cookieParser());
 
 // Mount routes to different paths
+app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes);
 app.use('/api/orgs', orgRoutes);
 app.use('/api/testimonials', testimonialRoutes);
 app.use('/api/services', serviceRoutes);
-app.use('/api/auth', authRoutes)
+app.use('/api/service-request', requestRoutes);
 app.use('/api/admin', authorizeRoles('admin'), adminRoutes)
 
 const PORT = process.env.PORT || 3000;
