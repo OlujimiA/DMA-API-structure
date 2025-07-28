3953c850e02f2b3df1f62d3d46a163e760955858
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-07-28 18:37:54 +0100

    Add formdata to api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authControllers = require('../controllers/authControllers');
 const userControllers = require('../controllers/userControllers');
+const formDataParser = require('../middlewares/multer')
 
 router.post('/refresh', authControllers.refreshToken)
-router.post('/login', authControllers.login);
+router.post('/login', formDataParser, authControllers.login);
 router.post('/register', userControllers.createUser);
 router.post('/forget-password', authControllers.forget_password);
 router.post('/forget-password/:token/:id', authControllers.reset_password);
