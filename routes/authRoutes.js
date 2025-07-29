b9391ed6a4f216afe645782aa6542bcaf982102e
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-07-29 17:31:04 +0100

    Add form-data parser to endpoints

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authControllers = require('../controllers/authControllers');
 const userControllers = require('../controllers/userControllers');
-const formDataParser = require('../middlewares/multer')
+const formDataParser = require('../middlewares/multer');
 
-router.post('/refresh', authControllers.refreshToken)
+router.post('/refresh-token', authControllers.refreshToken)
 router.post('/login', formDataParser, authControllers.login);
-router.post('/register', userControllers.createUser);
-router.post('/forget-password', authControllers.forget_password);
-router.post('/forget-password/:token/:id', authControllers.reset_password);
-router.post('/verify-email/:id', authControllers.verify_email);
-router.post('/resend-otp', authControllers.resend_otp);
+router.post('/register', formDataParser, userControllers.createUser);
+router.post('/forgot-password', formDataParser, authControllers.forgot_password);
+router.post('/reset-password/:token/:id', formDataParser, authControllers.reset_password);
+router.post('/verify-email/:id', formDataParser, authControllers.verify_email);
+router.post('/resend-otp', formDataParser, authControllers.resend_otp);
 module.exports = router;
 
