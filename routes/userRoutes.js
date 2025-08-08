f9f36d9d79cd36d336a407c7a6641bae93cfafd9
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-08-08 19:05:46 +0100

    Change upload field name

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const { authorizeRoles } = require('../middlewares/authenticate');
 const upload = require('../middlewares/multer');
 
 router.get('/', authorizeRoles('admin'), auth, userController.getAllusers);
-router.post('/profile/:id', upload.fields([ { name: 'profile-pic', maxCount: 1 }, { name: 'docs', maxCount: 5 } ]), auth, userController.profile);
+router.post('/profile/:id', upload.fields([ { name: 'profile-pic', maxCount: 1 }, { name: 'IDs', maxCount: 5 } ]), auth, userController.profile);
 router.get('/:id', auth, userController.getuserById);
 router.put('/:id', auth, upload.none(), userController.updateUser);
 router.delete('/:id', authorizeRoles('admin'), auth, userController.deleteUser);
