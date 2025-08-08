a1bcbd084908eaa6f7ab467d7c707948680b2df0
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-08-08 18:07:38 +0100

    Edit auth endpoints to use upload.none()
    formDataParser was previously used because multer
    middleware exported upload.none() solely.
    For multer to scale properly, formDataParser
    exports upload function.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const authControllers = require('../controllers/authControllers');
 const userControllers = require('../controllers/userControllers');
-const formDataParser = require('../middlewares/multer');
+const upload = require('../middlewares/multer');
 
 router.post('/refresh-token', authControllers.refreshToken)
-router.post('/login', formDataParser, authControllers.login);
-router.post('/register', formDataParser, userControllers.createUser);
-router.post('/forgot-password', formDataParser, authControllers.forgot_password);
-router.post('/reset-password/:token/:id', formDataParser, authControllers.reset_password);
-router.post('/verify-email/:id', formDataParser, authControllers.verify_email);
-router.post('/resend-otp', formDataParser, authControllers.resend_otp);
+router.post('/login', upload.none(), authControllers.login);
+router.post('/register', upload.none(), userControllers.createUser);
+router.post('/forgot-password', upload.none(), authControllers.forgot_password);
+router.post('/reset-password/:token/:id', upload.none(), authControllers.reset_password);
+router.post('/verify-email/:id', upload.none(), authControllers.verify_email);
+router.post('/resend-otp', upload.none(), authControllers.resend_otp);
 module.exports = router;
 
