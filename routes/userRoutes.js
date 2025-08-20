8c8e7f77702374d72d4e0a9548582ca040f9031d
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-08-20 13:11:54 +0100

    Import upload function in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../middlewares/authmiddleware');
 const userController = require('../controllers/userControllers');
 const { authorizeRoles } = require('../middlewares/authenticate');
+const upload = require('../middlewares/multer');
 
 router.get('/', authorizeRoles('admin'), auth, userController.getAllusers);
 router.post('/profile/:id', upload.fields([ { name: 'profile-pic', maxCount: 1 }, { name: 'IDs', maxCount: 5 } ]), auth, userController.profile);
