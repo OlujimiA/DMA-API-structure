c1308bee03c9f75e0e904fa6f6c3910b61c68197
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2025-08-24 17:38:08 +0100

    Add onUpdate fkey - NoAction

--- prisma/migrations/20250628150800_edit_service_model/migration.sql
+++ prisma/migrations/20250628150800_edit_service_model/migration.sql
@@ -6,8 +6,9 @@
   - Added the required column `subtitle` to the `service` table without a default value. This is not possible if the table is not empty.
 
 */
+
 -- DropForeignKey
--- ALTER TABLE `service` DROP FOREIGN KEY `fk_service_organisation_id`;
+ALTER TABLE `service` DROP FOREIGN KEY `fk_service_organisation_id`;
 
 -- DropIndex
 DROP INDEX `fk_service_organisation_id` ON `service`;
